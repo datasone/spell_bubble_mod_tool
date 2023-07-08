7969682e1cf194c82577b75a26e45dbebd179b0f
datasone <datasone@datasone.moe>
2023-07-08 15:37:28 +0800

    Update build.rs for .NET 7 NativeAOT

--- build.rs
+++ build.rs
@@ -2,16 +2,21 @@ fn main() {
     // TODO: All-In-One build.rs after project code is put together.
     println!("cargo:rustc-link-lib=user32");
     println!("cargo:rustc-link-lib=ole32");
-    println!("cargo:rustc-link-lib=bcrypt");
-    println!("cargo:rustc-link-lib=ncrypt");
-    println!("cargo:rustc-link-lib=crypt32");
-    println!("cargo:rustc-link-lib=iphlpapi");
+
+    println!("cargo:rustc-link-arg=/INCLUDE:NativeAOT_StaticInitialization");
+    println!(
+        "cargo:rustc-link-search=C:/Users/datasone/.nuget/packages/runtime.win-x64.microsoft.\
+         dotnet.ilcompiler/7.0.8/sdk"
+    );
+    println!(
+        "cargo:rustc-link-search=C:/Users/datasone/work/UnityAssetBundleHelper/\
+         SpellBubbleModToolHelper/bin/Release/net7.0/win-x64/publish"
+    );
 
     println!("cargo:rustc-link-lib=static=bootstrapperdll");
     println!("cargo:rustc-link-lib=static=Runtime.ServerGC");
+    println!("cargo:rustc-link-lib=static=System.Globalization.Native.Aot");
     println!("cargo:rustc-link-lib=static=System.IO.Compression.Native.Aot");
 
     println!("cargo:rustc-link-lib=static=SpellBubbleModToolHelper");
-
-    println!("cargo:rustc-link-search=./lib");
 }
