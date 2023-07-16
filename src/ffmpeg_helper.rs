26eb37dd411b99ac35e3086ed3bab3f1c48f28e2
datasone <datasone@datasone.moe>
2023-07-16 23:58:09 +0800

    Remove unused ffmpeg duration detection

--- src/ffmpeg_helper.rs
+++ src/ffmpeg_helper.rs
@@ -1,22 +1,4 @@
-use std::{error::Error, path::Path, process::Command};
-
-pub fn get_duration(file_path: &str) -> Result<f32, Box<dyn Error>> {
-    let output = Command::new("ffprobe")
-        .arg("-i")
-        .arg(file_path)
-        .arg("-show_entries")
-        .arg("format=duration")
-        .arg("-v")
-        .arg("quiet")
-        .arg("-of")
-        .arg("csv=p=0")
-        .output()?;
-
-    let duration = std::str::from_utf8(&output.stdout)?;
-    let duration = duration.trim_end().parse::<f32>()?;
-
-    Ok(duration)
-}
+use std::{path::Path, process::Command};
 
 pub fn convert_file(file_path: &Path, dest_path: &Path) -> std::io::Result<()> {
     Command::new("ffmpeg")
